2414608acbaf35feb18dfe1f86cdaa33f5a93c50
frankopkusianwar <okirorfrank3@gmail.com>
2020-10-18 12:45:42 +0300

    Import Hooks

--- src/containers/SpeciesList.js
+++ src/containers/SpeciesList.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import Species from '../components/Species'
 
 const SpeciesList = () => {
