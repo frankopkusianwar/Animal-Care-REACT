bf3e6e20e93809606b3d23e0f2a4e443ad7da255
frankopkusianwar <okirorfrank3@gmail.com>
2020-10-20 21:08:36 +0300

    Complete testing reducers

---
+++ src/__test__/reducers/species.test.js
@@ -0,0 +1,43 @@
+import speciesReducer from '../../reducers/species'
+
+describe('test species reducer', () => {
+  it('should return the initial state', () => {
+    expect(speciesReducer({ species: {species: [], allData: [], error: '',} }, {})).toEqual({
+      species: { species: [], allData: [], error: '',},
+    });
+  });
+
+  it('should update store when gets all speciess', () => {
+    expect(
+      speciesReducer(
+        undefined,
+        {
+          type: 'FETCH_SPECIES_SUCCESS',
+          payload: {phylum_name: 'phylum_name',}, 
+        },
+      ),
+    ).toEqual({
+      species: undefined,
+      allData: {phylum_name: 'phylum_name',},
+      error: "",
+    });
+  });
+
+  it('should update store when fails to get species', () => {
+    expect(
+      speciesReducer(
+        undefined,
+        {
+          type: 'FETCH_SPECIES_FAILURE',
+          error: {},
+          allData: []
+        },
+      ),
+    ).toEqual({
+      error: undefined,
+      species: [],
+      allData: []
+    });
+  });
+
+});
---
+++ src/__test__/reducers/threatReducer.test.js
@@ -0,0 +1,40 @@
+import threatsReducer from '../../reducers/threatsReducer'
+
+describe('test threats reducer', () => {
+  it('should return the initial state', () => {
+    expect(threatsReducer({ threats: [], error: '', }, {})).toEqual({
+      threats: [], error: '',
+    });
+  });
+  
+  it('should update store when gets threats', () => {
+    expect(
+      threatsReducer(
+        undefined,
+        {
+          type: 'FETCH_SPECIES_THREATS_REQUEST_SUCCESS',
+          payload: {title: 'soil erosion',}, 
+        },
+      ),
+    ).toEqual({
+      threats: undefined,
+      error: "",
+    });
+  });
+
+  it('should update store when fails to get threats', () => {
+    expect(
+      threatsReducer(
+        undefined,
+        {
+          type: 'FETCH_SPECIES_THREATS_REQUEST_FAILURE',
+          error: {},
+        },
+      ),
+    ).toEqual({
+      error: undefined,
+      threats: [],
+    });
+  });
+
+});
